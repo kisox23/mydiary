7c270a6d0cac5b6b662d20be28ec661dbe4c09dd
kisox23 <genialoide144@gmail.com>
2019-08-06 22:36:32 +0200

    darl

---
+++ darl/http/contenitore.php
@@ -0,0 +1,42 @@
+<html>
+<head><title>CONTENITORE</title>
+<link href="/favicon.ico" rel="icon">
+</head>
+<style>
+	html {display: table; margin: auto; height: 100%;}
+	h1 {text-align: center; margin-top: 50px; font-family: serif; font-variant: inherit; font-size: 30px;
+	font-family: monospace; }
+	body {background-color: lemonchiffon; display: table-cell; vertical-align: middle; height: 100%;}
+	input {font-style: oblique; background-color: lightpink; border-radius: 20px; border-width: 5px; border-color: black;
+	display: block; margin: auto; margin-top: 180px; height: 100px; width: 300px;
+	font-size: 40px; text-align: center; }
+	textarea {display: block; margin: 0 auto; border-width: 3px;}
+</style>
+<body>
+	<h1>Qui puoi scrivere tutto quello che ti passa per la testa!</h1>
+	<h1>A partire da lamentele a dichiarazioni, cose dolci, cose stronze, cose che ti succedono, osservazioni.</h1>
+	<form method="post" name="formm">
+	<textarea id="txtarea" cols="100" rows="5" name="txtarea"></textarea>
+	<input name="tochange" id="grr" type="submit" value="INVIA" onclick="change()"></form>
+</body>
+<script>
+	function change() {
+		document.getElementById("grr").name = "formm";
+	}
+</script>
+<?php
+if (isset($_POST['formm'])) {
+	savetxt();
+}
+function savetxt() {
+	$line = "\n<----------------------------------------------->";
+	$path = "/srv/http/cont.txt";
+	$file = fopen($path, "a");
+	$text = $_POST["txtarea"];
+	file_put_contents($path, $text, FILE_APPEND);
+	file_put_contents($path, $line, FILE_APPEND);
+	fclose($file);
+	echo "<script>window.close();</script>";
+}
+?>
+</html>
